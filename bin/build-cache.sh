22c13fba0ac2224ee5b2374f5fb43b891e7dcdb0
Gilg4mesh <freedomtomdestiny@gmail.com>
2019-11-10 17:52:31 +0800

    Remove loom-js & update cache path

--- bin/build-cache.sh
+++ bin/build-cache.sh
@@ -1,5 +1,5 @@
-mkdir -p DEXON_BBS_Cache/gh-pages/
-cp dist/cache.html DEXON_BBS_Cache/gh-pages/cache.html
+mkdir -p LOOM_BBS_Cache/gh-pages/
+cp dist/cache.html LOOM_BBS_Cache/gh-pages/cache.html
 npm run cache
-cp -R DEXON_BBS_Cache/dist/s dist/
-cp -R DEXON_BBS_Cache/dist/p dist/
+cp -R LOOM_BBS_Cache/dist/s dist/
+cp -R LOOM_BBS_Cache/dist/p dist/
